38980c2245f251a1c751e1815eaa773a18841674
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-21 11:12:41 -0700

    reorganized files based on feedback from central grading

--- Assets/script.js
+++ Assets/script.js
@@ -5,11 +5,25 @@
 
 const timerEl = document.querySelector(".timer-text");
 const startButton = document.querySelector(".start-button");
+const questionEl = document.querySelector(".quiz-area");
 
 const highScore = "";
 const myInitials = "";
 let timer;
 let timerCount = 75;
+let question1 = "Commonly used data types do NOT include: "
+
+function startGame () {
+startButton.disabled = true;
+startTimer();
+poseQuestion ();
+}
+
+
+function poseQuestion (){
+
+    questionEl.innerHTML = question1;
+}
 
 function startTimer() {
     // Sets timer
@@ -24,4 +38,4 @@ function startTimer() {
     }, 1000);
   }
 
-  startButton.addEventListener("click", startTimer);
+  startButton.addEventListener("click", startGame);
