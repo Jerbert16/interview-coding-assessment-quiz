c755cd50c76e2a34d27db9627793c5a8a67f30a6
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-20 19:58:31 -0700

    updated CSS, styling now correct

---
+++ Develop/script.js
@@ -0,0 +1,7 @@
+// Begin by creating array of questions
+// Then create an array of answers 
+//Create start quiz function
+//Create timer interval
+//Create each question as a function include buttons and listeners, if thens, display & count correct v false. Move on to next question.
+//Display final score, create enter initials form
+
