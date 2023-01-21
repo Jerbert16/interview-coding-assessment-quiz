c557c086e9dbd7b8c0022db3e55902a820e2c9dd
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-21 09:12:50 -0700

    tweaked CSS to include header. Got timer working

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,27 @@
-// Begin by creating array of questions
-// Then create an array of answers 
 //Create start quiz function
 //Create timer interval
 //Create each question as a function include buttons and listeners, if thens, display & count correct v false. Move on to next question.
 //Display final score, create enter initials form
 
+const timerEl = document.querySelector(".timer-text");
+const startButton = document.querySelector(".start-button");
+
+const highScore = "";
+const myInitials = "";
+let timer;
+let timerCount = 75;
+
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerEl.textContent = timerCount;
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
+
+  startButton.addEventListener("click", startTimer);
